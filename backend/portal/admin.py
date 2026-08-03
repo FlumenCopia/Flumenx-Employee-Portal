b3b4eb568174b4ba007967726b077ea0c0f067b3
nidhinkg <nidhinkg100@gmail.com>
2026-08-03 13:59:18 +0530

    changed

--- backend/portal/admin.py
+++ backend/portal/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import Announcement, AttendanceCorrection, AttendancePolicy, AttendanceRecord, AuditLog, Employee, LeaveRequest, Meeting, Notification, SalarySlip, UserRole
 
 admin.site.register([UserRole, Employee, LeaveRequest, SalarySlip, Meeting, Announcement, AttendancePolicy, AttendanceRecord, AttendanceCorrection, Notification, AuditLog])
+ 
